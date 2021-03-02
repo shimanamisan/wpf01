fd62fd09a63e2e5fe44d62977feeb18426e6a86e
shimanamisan <mikan.sup.all@gmail.com>
2021-03-02 16:03:19 +0900

    プロジェクト ファイルを追加します。

---
+++ wpf01/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace wpf01
+{
+    /// <summary>
+    /// Page1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 選択したデータがPage2クラスがインスタンス化されるときに渡される
+            Page2 page2 = new Page2(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(page2);
+        }
+    }
+}
---
+++ wpf01/Page2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf01
+{
+    /// <summary>
+    /// Page2.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        // カスタムコンストラクター
+        // Page1クラスから渡ってきたデータを受け取る
+        public Page2(object data) : this()
+        {
+            // bind to Page1 data
+            this.DataContext = data;
+        }
+    }
+}
